2bf0eac80510049b3d9bab0a28c671d2e6e2064b
Guilherme Campos <guicamppos12@gmail.com>
2026-06-07 19:27:48 -0300

    feat: hash na password

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { prisma } from "../lib/prisma";
 import { RegisterRequest } from "../types";
+import bcrypt from "bcrypt"
 
 export const registerUser = async (payload: RegisterRequest) => {
   const existingUser = await prisma.user.findUnique({
@@ -10,12 +11,14 @@ export const registerUser = async (payload: RegisterRequest) => {
     throw new Error("Email já cadastrado.");
   }
 
+  const hasedPassword = await bcrypt.hash(payload.password, 10)
+
   const newUser = await prisma.user.create({
     data: {
       firstName: payload.firstName,
       lastName: payload.lastName,
       email: payload.email,
-      password: payload.password,
+      password: hasedPassword,
       cpf: payload.cpf,
       birthdate: payload.birthDate ? new Date(payload.birthDate) : undefined,
       phone: payload.phone,
