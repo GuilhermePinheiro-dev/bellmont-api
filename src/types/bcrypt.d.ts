4a225526bf8995e6bce63d89993883859cef8bce
Guilherme Campos <guicamppos12@gmail.com>
2026-07-11 15:50:18 -0300

    fix: erro no deploy

---
+++ src/types/bcrypt.d.ts
@@ -0,0 +1,22 @@
+declare module "bcrypt" {
+  export function hash(
+    password: string,
+    salt: string | number,
+  ): Promise<string>;
+  export function compare(data: string, encrypted: string): Promise<boolean>;
+  export function genSalt(rounds?: number): Promise<string>;
+  export function hashSync(password: string, salt: string | number): string;
+  export function compareSync(data: string, encrypted: string): boolean;
+  export function genSaltSync(rounds?: number): string;
+
+  const bcrypt: {
+    hash: typeof hash;
+    compare: typeof compare;
+    genSalt: typeof genSalt;
+    hashSync: typeof hashSync;
+    compareSync: typeof compareSync;
+    genSaltSync: typeof genSaltSync;
+  };
+
+  export default bcrypt;
+}
