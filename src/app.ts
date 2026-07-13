a810c4886cceaa3e2bfbb1c5f322e68e1dde08ec
Guilherme Campos <guicamppos12@gmail.com>
2026-07-13 10:58:22 -0300

    feat: alteração na url do servidor

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import csrf from "@fastify/csrf-protection"
 
 const PORT = parseInt(process.env.PORT ?? "3000");
 const HOST = process.env.HOST ?? "0.0.0.0";
+const serverUrl = process.env.RENDER_EXTERNAL_URL || `http://localhost:${PORT}`
 
 const fastify = Fastify({
   logger: {
@@ -61,7 +62,7 @@ fastify.register(swagger, {
     },
     servers: [
       {
-        url: `http://localhost:${PORT}`,
+        url: serverUrl,
         description: "Servidor de desenvolvimento",
       },
     ],
